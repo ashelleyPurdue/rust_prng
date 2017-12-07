e58627060f4166d78ceff59a4bf075bc618998da
Alex Shelley <ashelley@purdue.edu>
2017-12-07 14:12:33 -0500

    separated RandGenStackOverflow into a separate module, but the compiler isn't liking it yet.

--- src/random/mod.rs
+++ src/random/mod.rs
@@ -5,33 +5,37 @@ pub trait RandGen
     fn next_u64(&mut self, max: u64) -> u64;
 }
 
-pub struct RandGenStackOverflow
+mod rand_gen_stack_overflow
 {
-    seed: u64,
-    state: u64
-}
-
-impl RandGenStackOverflow
-{
-    // Constructor
-    pub fn new(seed: u64) -> RandGenStackOverflow
+    pub struct RandGenStackOverflow
     {
-        RandGenStackOverflow
+        seed: u64,
+        state: u64
+    }
+
+
+    impl RandGenStackOverflow
+    {
+        // Constructor
+        pub fn new(seed: u64) -> RandGenStackOverflow
         {
-            seed: seed,
-            state: seed
+            RandGenStackOverflow
+            {
+                seed: seed,
+                state: seed
+            }
         }
     }
-}
 
-impl RandGen for RandGenStackOverflow
-{
-    fn next_u64(&mut self, max: u64) -> u64
+    impl RandGen for RandGenStackOverflow
     {
-        let output = hashing::stack_overflow_hash(self.state);         // Hash the state for our output
-        self.state = self.state.wrapping_add(output);   // Do something to the state to make
-                                                        // the next output different.
-        return output % max;
+        fn next_u64(&mut self, max: u64) -> u64
+        {
+            let output = hashing::stack_overflow_hash(self.state);         // Hash the state for our output
+            self.state = self.state.wrapping_add(output);   // Do something to the state to make
+                                                            // the next output different.
+            return output % max;
+        }
     }
 }
 
