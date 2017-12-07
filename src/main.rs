401652bb0e69f7aa76e012b400a496857333fa55
Alex Shelley <ashelley@purdue.edu>
2017-12-07 13:20:11 -0500

    Added function for displaying the frequency graph of a RandGen's output.

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,37 @@ fn main()
     // Make a new RandGen
     let mut rand_gen = RandGen::new(1337);
 
-    // Print 10 random numbers
-    for _ in 0..10
+    // Print its frequency graph
+    print_frequency_graph(&mut rand_gen, 10, 100);
+}
+
+fn print_frequency_graph(rand_gen: &mut RandGen, max_num: u64, num_trials: u64)
+{
+    // Generates (num_trials) random numbers between 0 and (max_num)
+    // Prints out a frequency graph
+
+    let mut frequencies = vec![0; max_num as usize];
+
+    // Generate all the numbers and tally their frequencies
+    for _ in 0..num_trials
     {
-        println!("{}", rand_gen.next_u64(10));
+        let num = rand_gen.next_u64(max_num) as usize;
+        frequencies[num] += 1;
+    }
+
+    // Print the graph
+    for num in 0..max_num
+    {
+        // Print which number this is
+        print!("{}:\t", num);
+
+        // Print a star for each time it appeared
+        for _ in 0..frequencies[num as usize]
+        {
+            print!("*");
+        }
+
+        // Finish the line
+        println!("");
     }
 }
